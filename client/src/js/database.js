4c2fa385a5dd79c8c2476ab4c94790549553c94a
ggrove87 <gwg1387@gmail.com>
2022-05-25 14:52:45 -0500

    Initial Commit

---
+++ client/src/js/database.js
@@ -0,0 +1,29 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.error('Put to the object store');
+  const jate_db = await openDB('jate_db', 1);
+  const tx = jate_db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, jate: content});
+  const result = await request;
+}
+
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => console.error('getDb not implemented');
+
+initdb();
